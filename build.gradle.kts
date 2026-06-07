cc7f87d048b9abd95f57bbb39a75eb6b230c6b05
AlexSkabich <skabichevsky.alex@gmail.com>
2026-06-07 15:46:43 +0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version "8.5.1" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false // REQUIRED for Kotlin 2.0.0
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.24" apply false // Updated to 1.0.24
+    id("com.google.devtools.ksp") version "2.0.0-1.0.24" apply false 
 }
